b4ed63407cc1eb1618c5e162a2115f5bf7539a39
Hadeel Yassin <hadeelyassin1997@gmail.com>
2021-01-30 17:06:21 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo</h1>
+      <p><strong>thing i need to do</strong> </p>
+      <ul>
+        <li>learn react</li>
+        <li>climb mt everst</li>
+        <li>run a marathon</li>
+        <li>feed the dogs</li>
+      </ul>
+
+
+    </div>
+  );
+}
+
+export default App;
